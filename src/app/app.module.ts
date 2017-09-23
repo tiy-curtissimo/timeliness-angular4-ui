84a6b6d4243cc7327e10e61802fc8ec44e5bc225
Curtis Schlak <curtis.schlak@theironyard.com>
2017-09-23 13:01:15 -0700

    Added 13-link-based-routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { RegistrationCtaComponent } from './registration-cta/registration-cta.component';
 import { LoginCardComponent } from './login-card/login-card.component';
 import { SignUpCardComponent } from './sign-up-card/sign-up-card.component';
 import { LoginPageComponent } from './login-page/login-page.component';
+
+const appRoutes: Routes = [
+  { path: 'login',  component: LoginPageComponent },
+  { path: 'signup', component: SignUpCardComponent },
+  { path: '',  redirectTo: '/login', pathMatch: 'full' }
+];
 
 @NgModule({
   declarations: [
@@ -20,7 +27,8 @@ import { LoginPageComponent } from './login-page/login-page.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
